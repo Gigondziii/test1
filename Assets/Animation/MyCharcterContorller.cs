59094bb1b76aa74e4a4d6cd96a60cabf42b12b25
Gigondziii <giosakhvadze01@gmail.com>
2025-02-27 05:51:46 -0800

    terrains

---
+++ Assets/Animation/MyCharcterContorller.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class MyCharcterContorller : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    public Animator controller;
+
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.W))
+//        {
+//            controller.SetBool("IsRunning", true);
+
+           
+//        }  
+        
+//        else 
+
+
+
+
+
+//    }
+//}
